1b41f935e412e8ad7291fe7670551ce7a2317d8a
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-14 17:09:48 -0500

    added Navigation component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navigation from "./components/Navigation";
 import About from "./components/About";
 // Error in terminal that "logo is defined but never used", but if the line below is removed, no images render on the page
 import logo from "./logo.svg";
@@ -7,6 +8,7 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <Navigation></Navigation>
       <h1>Kristy Nerhaugen's Portfolio</h1>
       <main>
         <About></About>
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex-row">
+          <li>
+            <a href="#about">About Kristy</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
