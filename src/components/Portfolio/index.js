8b378ca7e644714e70db38adabeb52970b5e644a
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-16 16:32:37 -0500

    imported images to projects list

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import screenshot from "../../assets/images/BoozyBooks.png";
+import boozyBooks from "../../assets/images/BoozyBooks.png";
+import PARKOUR from "../../assets/images/PARKOUR.png";
+import noteTaker from "../../assets/images/KNNoteTaker.png";
+import techBlog from "../../assets/images/KNTechBlog.png";
+import textEditor from "../../assets/images/KNTextEditor.png";
+import passwordGenerator from "../../assets/images/passwordGenerator.png";
 
 const Portfolio = () => {
   const [projects] = useState([
@@ -8,24 +13,28 @@ const Portfolio = () => {
       description: "Group Project #2",
       link: "https://parkour-review.herokuapp.com/",
       github: "https://github.com/KristyNerhaugen/playground-rating-app",
+      image: PARKOUR,
     },
     {
       name: "Boozy Books",
       description: "Group Project #1",
       link: "https://kristynerhaugen.github.io/Boozy-Books/",
       github: "https://github.com/KristyNerhaugen/Boozy-Books",
+      image: boozyBooks,
     },
     {
       name: "KN Note Taker",
       description: "A note taking application.",
       link: "https://kn-note-taker.herokuapp.com/",
       github: "https://github.com/KristyNerhaugen/kn-note-taker",
+      image: noteTaker,
     },
     {
       name: "KN Tech Blog",
       description: "A blog site for technology enthusiast.",
       link: "https://kn-tech-blog.herokuapp.com/",
       github: "https://github.com/KristyNerhaugen/kn-tech-blog",
+      image: techBlog,
     },
     {
       name: "KN Text Editor",
@@ -33,12 +42,14 @@ const Portfolio = () => {
         "Create notes or code snippets with or without an internet connection.",
       link: "https://hidden-gorge-32319.herokuapp.com/",
       github: "https://github.com/KristyNerhaugen/kn-text-editor",
+      image: textEditor,
     },
     {
       name: "Password Generator",
       description: "Generate a random password.",
       link: "https://kristynerhaugen.github.io/password-generator/",
       github: "https://github.com/KristyNerhaugen/password-generator",
+      image: passwordGenerator,
     },
   ]);
 
@@ -52,12 +63,7 @@ const Portfolio = () => {
             <p>{project.description}</p>
             <a href={project.link}>{project.name}</a>
             <a href={project.github}>Github Repository</a>
-            <img
-              src={require(`../../assets/images/${project.name}.png`).default}
-              alt={project.name}
-              className="img-thumbnail mx-1"
-              key={project.name}
-            />
+            <img src={project.image} alt={project.name} />
           </div>
         ))}
       </div>
