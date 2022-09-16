29764ef825e3dfc150e355d39aa51daf93c546c8
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-16 12:31:53 -0500

    added in css

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import screenshot from "../../assets/images/BoozyBooks.png";
 
 function Project(props) {
-  const currentProj = {
+  const currentProject = {
     name: "Boozy Books",
     description: "Group Project #1",
+    link: "https://kristynerhaugen.github.io/Boozy-Books/",
+    github: "https://github.com/KristyNerhaugen/Boozy-Books",
   };
   return (
     <section>
-      <h1>{currentProj.name}</h1>
-      <p>{currentProj.description}</p>
+      <h1>{currentProject.name}</h1>
+      <p>{currentProject.description}</p>
+      <a href={currentProject.link}>View deployed {currentProject.name}!</a>
+      <a href={currentProject.github}>
+        View GitHub repositiory for {currentProject.name}.
+      </a>
       <div className="flex-row">
         <img
           src={screenshot}
