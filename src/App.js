b5fe01a533c716531c9763f0b303d94785bf3d96
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-16 15:05:06 -0500

    contact form working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "./components/About";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
 
       <main>
         <About></About>
+        <Contact></Contact>
       </main>
 
       <Footer></Footer>
