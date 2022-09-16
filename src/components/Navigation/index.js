c73f71948794c97706b494ece07191f1e63837ae
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-16 16:06:28 -0500

    trying to get navigation links working

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,20 +9,29 @@ function Navigation(props) {
     return item;
   };
 
+  const {
+    contactSelected,
+    setContactSelected,
+    portfolioSelected,
+    setPortfolioSelected,
+    resumeSelected,
+    setResumeSelected,
+  } = props;
+
   return (
     <nav>
       <ul className="flex-row">
         <li className="mx-2">
           <a href="#about">About Kristy</a>
         </li>
-        <li className="mx-2">
-          <span onClick={() => handleClick("Portfolio")}>Portfolio</span>
+        <li className={`mx-2 ${portfolioSelected && "navActive"}`}>
+          <span onClick={() => setPortfolioSelected(true)}>Portfolio</span>
         </li>
-        <li className="mx-2">
-          <span onClick={() => handleClick("Contact")}>Contact</span>
+        <li className={`mx-2 ${contactSelected && "navActive"}`}>
+          <span onClick={() => setContactSelected(true)}>Contact</span>
         </li>
-        <li className="mx-2">
-          <span onClick={() => handleClick("Resume")}>Resume</span>
+        <li className={`mx-2 ${resumeSelected && "navActive"}`}>
+          <span onClick={() => setResumeSelected(true)}>Resume</span>
         </li>
       </ul>
     </nav>
