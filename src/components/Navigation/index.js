5510045706445d6ebccf5974a116878790479316
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-17 09:16:16 -0500

    trying to get link highlighting to work on navigation, still not working

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,12 +4,13 @@ function Navigation(props) {
   // const [activeNav, setActiveNav] = useState("About");
 
   const { setActiveNav, pageList } = props;
+  console.log(pageList.name);
 
   return (
     <nav>
       <ul className="flex-row">
         {pageList.map((page) => (
-          <li key={page} className={`mx=2`}>
+          <li key={page} className={`mx-2`}>
             <span onClick={() => setActiveNav(page)}>{page}</span>
           </li>
         ))}
