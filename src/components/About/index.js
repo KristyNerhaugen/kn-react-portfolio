c7a68af84557c935c352525f8c406ceb82ca804f
kristynerhaugen <kristynerhaugen@gmail.com>
2022-11-19 17:16:57 -0600

    updates to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,15 +7,16 @@ function About() {
       <h1 id="aboutMe">About Kristy</h1>
       <img src={kristyPhoto} style={{ width: "20%" }} alt="kristy" />
       <p>
-        Kristy Nerhaugen is a student in the University of Minnesota’s Coding
-        Boot Camp. Kristy has worked in libraries for over ten years and is
-        excited to pivot careers with all of the new skills she’s learning
-        through the Boot Camp! She has a Master's degree in Library Science from
-        the University of Indiana Bloomington and a Bachelor’s degree in English
-        from Gustavus Adolphus College. Kristy has strong organization skills
-        and excels in communication and customer service. If you would like to
-        get in touch with Kristy, please use the contact form. Enjoy exploring
-        the projects Kristy has developed!
+        Kristy Nerhaugen is a full stack developer. She recently received her
+        full stack web development certificate from the University of
+        Minnesota’s Coding Boot Camp. Kristy has worked in libraries for over
+        ten years and is excited to pivot careers with all of the new skills she
+        learned throughout the Boot Camp! She has a Master's degree in Library
+        Science from the University of Indiana Bloomington and a Bachelor’s
+        degree in English from Gustavus Adolphus College. Kristy has strong
+        organization skills and excels in communication and customer service. If
+        you would like to get in touch with Kristy, please use the contact form.
+        Enjoy exploring the projects Kristy has developed!
       </p>
     </section>
   );
